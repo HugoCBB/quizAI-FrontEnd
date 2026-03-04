023f88b2951045d51a4532fc56268dfd09a45e8e
Hugo <157296697+HugoCBB@users.noreply.github.com>
2026-03-04 16:59:04 -0300

    Update API URL for question fetching

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const formSubmit = () => {
 
 
 const generateCard = async ({ theme, question_quantity }) => {
-      const url = "https://api-quizai-production.up.railway.app/question/";
+      const url = "https://api-quizai-1.onrender.com/question/";
 
       try {
         const response = await fetch(url, {
